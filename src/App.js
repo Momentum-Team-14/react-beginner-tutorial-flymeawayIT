b8d62f9b960e23c42b6148112cf11259f0d67b26
Gary Whitford <ssrdunc@gmail.com>
2022-09-08 14:38:41 -0400

    Team assisted in class to correct code

--- src/App.js
+++ src/App.js
@@ -5,17 +5,14 @@ import ColorChangeButton from './ColorChangeButton';
 function App() {
   const [color, setColor] = useState('')
   return (
-    <div>
-      {color}
       <div className={`react-root ${color}`}>
-        <header className="centered">
+        <div className="centered">
           <h1>Color Picker</h1> 
           <ColorChangeButton color='red' setColor={setColor} />
           <ColorChangeButton color='blue' setColor={setColor} />
           <ColorChangeButton color='yellow' setColor={setColor} />
-          </header>
+          </div>
       </div>
-    </div>
   );
 }
 
--- src/ColorChangeButton.js
+++ src/ColorChangeButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ColorChangeButton(props) {
     return (
-        <button className={props.color} onClick={() => props.setColor(props.Color)}>{props.color}</button>
+        <button className={props.color} onClick={() => props.setColor(props.color)}>{props.color}</button>
     )
 }
 
